2ec3e77ce94868ac59e957670c7a0e98aef877a9
Lucas Hörand <lucas.horand@gmail.com>
2022-04-04 11:07:32 +0200

    removed crappy tooltip lib

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-//need to get rid of this crappy typeless plugin
-import FloatingVue from 'floating-vue'
-import 'v-tooltip/dist/v-tooltip.css'
-
 createApp(App)
-    .use(FloatingVue)
     .mount('#app')
